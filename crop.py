81afc25e31ee1abda78855817ceb9a42cd1c153f
mgrentzer <grentzer.2@buckeyemail.osu.edu>
2023-10-02 11:55:49 -0400

    Created Crop Tool w/ Ex.

---
+++ crop.py
@@ -0,0 +1,15 @@
+from PIL import Image
+
+class CropTool:
+    @classmethod
+    def crop(cls, img_name: str, top: int, right: int, bottom: int, left: int)-> Image:
+        image = Image.open(img_name)
+        cropped_image = image.crop((left, top, right, bottom))
+        image.close()
+        return cropped_image
+
+def test():
+    img = CropTool.crop("example.gif", 167, 956, 633, 44)
+    img.save("output.gif")
+
+test()
