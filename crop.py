0e54f4e66a1738d6b4c6ff176673924aa4912b10
mgrentzer <grentzer.2@buckeyemail.osu.edu>
2023-10-02 11:57:46 -0400

    Created Crop

--- crop.py
+++ crop.py
@@ -3,7 +3,7 @@ from PIL import Image
 class CropTool:
     @classmethod
     def crop(cls, img_name: str, top: int, right: int, bottom: int, left: int)-> Image:
-        image = Image.open(img_name)
+        image = Image.open(img_name) 
         cropped_image = image.crop((left, top, right, bottom))
         image.close()
         return cropped_image
